30fff0fa4d83d1483aff8f7b6877b5f7cb6e0925
Franco Di Leo <dileo.francoj@gmail.com>
2024-06-19 19:40:54 -0300

    chore: test in lintstagedrc

--- src/hooks/useHello.ts
+++ src/hooks/useHello.ts
@@ -3,6 +3,7 @@ import * as React from 'react'
 export const useHello = (): unknown => {
   const [state, setState] = React.useState('')
   console.log('in useHello hook')
+
   return {
     state,
     setState
